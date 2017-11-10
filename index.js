91304859a86294714b544e3d5e2303bd1e382752
nicholas kunz <mrnicholaskunz@gmail.com>
2017-11-10 17:21:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-// Code your solutions in this file
-const employees = ['Lisa', 'Kaitlin', 'Jan'];
-function printBadges(employees) {
-  for (let i=0; i < employees.length; i++) {
-    console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
-    }
-    return employees;
-}
-
-// let i=0
-let n=0;
-function tailsNeverFails() {
-  return Math.random() >= 0.5;
-  }
-
-while (tailsNeverFails()) {
-  console.log(`You got ${n++} tails in a row!`);
-}
-
-
-
-
-//       i++;
+// // Code your solutions in this file
+// const employees = ['Lisa', 'Kaitlin', 'Jan'];
+// function printBadges(employees) {
+//   for (let i=0; i < employees.length; i++) {
+//     console.log(`Welcome ${employees[i]}! You are employee #${i+1}.`);
 //     }
+//     return employees;
 // }
-//   console.log(`You got ${i} tails in a row!`);
+// 
+// // let i=0
+// let n=0;
+// function tailsNeverFails() {
+//   return Math.random() >= 0.5;
+//   }
+// 
+// while (tailsNeverFails()) {
+//   console.log(`You got ${n++} tails in a row!`);
+// }
+// 
+// 
+// 
+// 
+// //       i++;
+// //     }
+// // }
+// //   console.log(`You got ${i} tails in a row!`);
